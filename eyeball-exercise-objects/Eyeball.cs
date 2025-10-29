2daf2f42092acc2e1ff881e91963eb1f1497badd
Mohawk <Mohawk@MICHAEL>
2025-10-28 20:14:22 -0400

    Eyeball game

---
+++ eyeball-exercise-objects/Eyeball.cs
@@ -0,0 +1,94 @@
+﻿using MohawkGame2D;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MohawkGame2D
+{
+    public class Eyeball
+    {
+        public float radius = 50.0f;
+        public Color pupilColor = Color.Black;
+        public Vector2 position = new Vector2(0, 0);
+
+        public bool closed = false;
+
+        public void Setup()
+        {
+            closed = false;
+            radius = Random.Float(20.0f, 50.0f);
+            position = Random.Vector2(50, 550, 50, 550);
+            pupilColor = Random.Color();
+        }
+
+        public void Update()
+        {
+            ProcessInputs();
+            DrawEyeball();
+        }
+
+        public bool IsMouseInside()
+        {
+            Vector2 eyeballPosToMousePos = Input.GetMousePosition() - position;
+            bool insideCircle = eyeballPosToMousePos.Length() < radius;
+            return insideCircle;
+        }
+
+        void ProcessInputs()
+        {
+            if (IsMouseInside() && Input.IsMouseButtonPressed(MouseInput.Left))
+            {
+                closed = true;
+            }
+        }
+
+        void DrawEyeball()
+        {
+            Draw.LineSize = 1;
+            Draw.LineColor = Color.Black;
+            Draw.FillColor = Color.White;
+
+            Draw.Circle(position, radius);
+
+            Vector2 mouseDirection = Input.GetMousePosition() - position;
+
+            float pupilRadius = radius / 2.0f;
+
+            Vector2 pupilPosition = new Vector2(0, 0);
+            if (mouseDirection.Length() > radius - pupilRadius)
+            {
+                // outside, the pupil only follows to a certain distance
+                pupilPosition = (radius / 2.0f) * Vector2.Normalize(mouseDirection);
+                pupilPosition += position;
+            }
+            else
+            {
+                // when the mouse is inside the eyeball, just make the pupil follow the mouse
+                pupilPosition = Input.GetMousePosition();
+            }
+            
+            // eye closing logic
+            if (closed)
+            {
+                Draw.FillColor = Color.LightGray;
+                Draw.Circle(position, radius);
+
+                Draw.Line(position - new Vector2(radius, 0), position + new Vector2(radius, 0));
+            }
+            else
+            {
+                // draw iris
+                Draw.FillColor = pupilColor;
+                Draw.Circle(pupilPosition, pupilRadius);
+
+                // draw pupil
+                Draw.FillColor = Color.Black;
+                Draw.Circle(pupilPosition, 0.60f * pupilRadius);
+            }
+
+        }
+    }
+}
--- eyeball-exercise-objects/Game.cs
+++ eyeball-exercise-objects/Game.cs
@@ -1,5 +1,5 @@
 ﻿// Include the namespaces (code libraries) you need below.
-using System;
+using System.Diagnostics.Metrics;
 using System.Numerics;
 
 // The namespace your code is in.
@@ -11,14 +11,23 @@ namespace MohawkGame2D
     public class Game
     {
         // Place your variables here:
-
+        Eyeball[] eyeballs = new Eyeball[10];
+        bool gameWon = false;
 
         /// <summary>
         ///     Setup runs once before the game loop begins.
         /// </summary>
         public void Setup()
         {
+            Window.SetTitle("Eyeballs");
+            Window.SetSize(600, 600);
+            Window.TargetFPS = 60;
 
+            for (int i = 0; i < eyeballs.Length; i++)
+            {
+                eyeballs[i] = new Eyeball();
+                eyeballs[i].Setup();
+            }
         }
 
         /// <summary>
@@ -26,8 +35,52 @@ namespace MohawkGame2D
         /// </summary>
         public void Update()
         {
+            Window.ClearBackground(Color.White);
 
+            int eyeClosedCounter = 0;
+
+            // update the eyeballs
+            bool resetPressed = Input.IsKeyboardKeyPressed(KeyboardInput.Space);
+            for (int i = 0; i < eyeballs.Length; i++)
+            {
+                // protect against null references
+                if (eyeballs[i] == null)
+                {
+                    continue;
+                }
+
+                // right click to remove the eyeball
+                if (Input.IsMouseButtonPressed(MouseInput.Right) && eyeballs[i].IsMouseInside())
+                {
+                    // remove the eyeball
+                    eyeballs[i] = null;
+                    continue;
+                }
+
+                if (gameWon && resetPressed)
+                {
+                    eyeballs[i].Setup();
+                }
+                eyeballs[i].Update();
+                if (eyeballs[i].closed)
+                {
+                    eyeClosedCounter++;
+                }
+            }
+
+            if (gameWon && resetPressed) gameWon = false;
+
+            Text.Draw($"Counter: {eyeClosedCounter}", new Vector2(10, 10));
+
+            // win condition
+            if (eyeClosedCounter == eyeballs.Length)
+            {
+                gameWon = true;
+                Text.Draw("YOU WON!!! WOW", new Vector2(200, 280));
+                Text.Draw("Press Spacebar to reset", new Vector2(200, 350));
+            }
         }
+
     }
 
 }
